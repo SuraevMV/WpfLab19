cc0b34c9c4565210250aaa16f6a4350639d3b655
SuraevMV <96659813+SuraevMV@users.noreply.github.com>
2022-03-23 21:08:01 +0300

    Добавьте файлы проекта.

---
+++ WpfLab19/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLab19.Models
+{
+    static class Ariph
+    {
+        public static double Add(double a) => 2 * a * Math.PI;
+    }
+}
